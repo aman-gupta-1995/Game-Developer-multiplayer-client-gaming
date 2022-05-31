6efb6db7005ca80ec66c8ddf0b7ba4f994b893af
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 15:26:25 +0530

    Update ChooseImage.java

--- Game Developer- multiplayer client gaming [EA Games, Hydearbad]/ShoddySense/src/shoddysense/ChooseImage.java
+++ Game Developer- multiplayer client gaming [EA Games, Hydearbad]/ShoddySense/src/shoddysense/ChooseImage.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2007 Cathy Fitzpatrick <cathy@cathyjf.com>
  * Created in March 2007.
  *  
- * This program is free software: you can redistribute it and/or modify   
+ * This program is free software: you can redistribute it and/or modify    
  * it under the terms of the GNU Affero General Public License as published by   
  * the Free Software Foundation, either version 3 of the License, or  
  * (at your option) any later version. 
