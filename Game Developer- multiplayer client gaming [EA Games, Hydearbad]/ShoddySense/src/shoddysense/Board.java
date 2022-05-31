0b4a76984c2db45adaf034f0cc069e228a2154f2
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 15:24:43 +0530

    Update Board.java

--- Game Developer- multiplayer client gaming [EA Games, Hydearbad]/ShoddySense/src/shoddysense/Board.java
+++ Game Developer- multiplayer client gaming [EA Games, Hydearbad]/ShoddySense/src/shoddysense/Board.java
@@ -4,7 +4,7 @@
  * Created in March 2007.
  *  
  * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Affero General Public License as published by  
+ * it under the terms of the GNU Affero General Public License as published by   
  * the Free Software Foundation, either version 3 of the License, or     
  * (at your option) any later version. 
  *
